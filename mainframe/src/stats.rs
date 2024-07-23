039ff7fc3fbae0fdbd61ddf8c808258b459a12d7
Thomas Schollenberger <Thomas.Schollenberger@collins.com>
2024-07-23 16:53:46 -0500

    start progress towards caching

--- mainframe/src/stats.rs
+++ mainframe/src/stats.rs
@@ -4,8 +4,22 @@ use crate::{database, discord};
 use anyhow::Result;
 use chrono::Utc;
 
+use std::sync::{Mutex, OnceLock};
+
+static LB: OnceLock<Mutex<Vec<(String, i32)>>> = OnceLock::new();
+fn cached_lb() -> &'static Mutex<Vec<(String, i32)>> {
+    LB.get_or_init(|| Mutex::new(vec![]))
+}
+
 pub async fn weekly_activity_lb(webhook: String, url: String, token: String) -> Result<()> {
     let top_10_names = database::get_top(url, token, 10).await?;
+    let mut cached = cached_lb().lock().unwrap();
+    if top_10_names == *cached {
+        return Ok(());
+    } else {
+        *cached = top_10_names.clone();
+    }
+
     println!("got top {top_10_names:?}");
 
     let description = top_10_names
